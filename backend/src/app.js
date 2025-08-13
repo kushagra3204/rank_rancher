308a7c754aaa452125da9c82e3f3ed5f87ee7365
Kushagra Agarwal <kushagra3204@gmail.com>
2025-08-13 11:52:06 +0530

    limit size increased to 500MB

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({ limit: '100mb' }))
+app.use(express.json({ limit: '500mb' }))
 app.use(cookieParser());
 app.use('/api',routes);
 
